1ae46528763d1a29fb8d8bd6616e8c712a1ffff3
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-20 19:53:55 +0400

    feat: add ws server

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
-import { httpServer } from './src/http_server/index.js';
+import { httpServer as server } from './src/http_server/index.js';
+import { WebSocketServer } from 'ws';
 
-const HTTP_PORT = 8181;
+const HTTP_PORT = 3000;
+
+export const wss = new WebSocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+});
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+server.listen(HTTP_PORT);
