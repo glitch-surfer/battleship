05751a2f26261d21078ea2c04b2cc5dae2b58593
Vladislav Garaev <vladislav.garaev@syngenta.com>
2024-10-26 23:29:04 +0400

    feat: handle disconnection

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { httpServer as server } from './src/http_server/index.js';
 import { WebSocketServer } from 'ws';
 import { wsMessageHandler } from './src/ws-handlers';
+import { socketsDb } from './src/db/sockets-db';
 
 const PORT = 3000;
 
@@ -10,7 +11,13 @@ wss.on('connection', (ws) => {
   console.log(`Client connected to the ${PORT} websocket port!`);
   ws.on('error', console.error);
   ws.on('message', (data) => wsMessageHandler(data.toString(), ws));
-});
+  ws.on('close', () => {
+      console.log('Client disconnected!');
+      socketsDb.removeSocket(ws);
+    },
+  );
+})
+  .on('error', console.error);
 
 console.log(`Start static http server on the ${PORT} port!`);
 server.listen(PORT);
